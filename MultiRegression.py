f223a08986ead8b6c2b2202e40d6e1a86dff2432
Rupin Jairaj <rupinjairaj@gmail.com>
2016-11-26 13:02:00 +0530

    Plotting various combinations of the dependent variables and the independent variables

--- MultiRegression.py
+++ MultiRegression.py
@@ -1,6 +1,7 @@
 #!usr/bin/python
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 b_data = pd.read_csv('Data/basketball.csv')
 
@@ -9,3 +10,32 @@ print b_data.describe()
 
 # Pearson correlation
 print b_data.corr()
+
+# Plotting the distribution of independent variables with respect to the dependent variables 
+## Plotting the distribution b/w height and avg points scored
+fig, ax = plt.subplots(1, 1)
+ax.scatter(b_data.height, b_data.avg_points_scored)
+ax.set_xlabel('height')
+ax.set_ylabel('Average points scored per game')
+plt.show()
+
+## Plotting the distribution b/w weight and avg points scored
+fig, ax = plt.subplots(1, 1)
+ax.scatter(b_data.weight, b_data.avg_points_scored)
+ax.set_xlabel('weight')
+ax.set_ylabel('Average points scored per game')
+plt.show()
+
+## Plotting the distribution b/w successful field goals and avg points scored
+fig, ax = plt.subplots(1, 1)
+ax.scatter(b_data.success_field_goals, b_data.avg_points_scored)
+ax.set_xlabel('Success Field Goals')
+ax.set_ylabel('Average points scored per game')
+plt.show()
+
+## Plotting the distribution b/w successful free throws and avg points scored
+fig, ax = plt.subplots(1, 1)
+ax.scatter(b_data.success_free_throws, b_data.avg_points_scored)
+ax.set_xlabel('Success free throws')
+ax.set_ylabel('Average points scored per game')
+plt.show()
